78f9bc2e788828bb2d6f779d70de34968de7d7e6
Morry28 <36274282+Morry28@users.noreply.github.com>
2024-01-08 23:57:39 +0100

    Update state.rs

--- contracts/counter/src/state.rs
+++ contracts/counter/src/state.rs
@@ -1,13 +1,25 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
-
-use cosmwasm_std::Addr;
+use cosmwasm_std::{Addr, Uint128};
 use cw_storage_plus::Item;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub enum BetSide {
+    Heads,
+    Tails,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+pub struct Bet {
+    pub bettor: Addr,
+    pub amount: Uint128,
+    pub side: BetSide,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+    pub head_bets: Vec<Bet>,
+    pub tail_bets: Vec<Bet>,
 }
 
 pub const STATE: Item<State> = Item::new("state");
